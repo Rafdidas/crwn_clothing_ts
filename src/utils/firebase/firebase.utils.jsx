8726cb357daa6fd074c31c21488190828e2d9943
hue <hue@wisa.co.kr>
2024-09-13 16:40:59 +0900

    firebase setting

---
+++ src/utils/firebase/firebase.utils.jsx
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJeVOLyj55ZKM8tAdwf08X8T-Bys7wl9I",
+  authDomain: "crwn-clothing-db-a76bc.firebaseapp.com",
+  projectId: "crwn-clothing-db-a76bc",
+  storageBucket: "crwn-clothing-db-a76bc.appspot.com",
+  messagingSenderId: "49833067567",
+  appId: "1:49833067567:web:9c42819ac68d39afa132da",
+};
+
+const firebaseapp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: 'select_account'
+});
