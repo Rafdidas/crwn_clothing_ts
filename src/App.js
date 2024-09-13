54738723dfe8c5a17e6981054da272c9fd3d6440
hue <hue@wisa.co.kr>
2024-09-13 15:11:25 +0900

    firebase utils

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import Home from './routes/home/home.component'
 import Navigation from './routes/navigation/navigation.component';
 import { Routes, Route } from 'react-router-dom';
+import SignIn from './routes/sign-in/sign-in.component';
 
-
-
-
-const Shop = () => {
-  return(
-    <div>
-      <h1>Shop</h1>
-    </div>
-  );
-}
 
 const App = () => {
 
@@ -19,7 +10,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation/>} >
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop/>} />
+        <Route path='sign-in' element={<SignIn/>} />
       </Route>
     </Routes>
   );
