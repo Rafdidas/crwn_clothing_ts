e6d593f0e665f4fd2998a6ec325f1ba6409ae09c
HyeonGyu <yhu930421@naver.com>
2024-09-21 23:19:30 +0900

    email 로그인 방법  추가

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,22 +3,23 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpform from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
-    
+
     const logGoogleUser = async () => {
         const {user} = await signInWhithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     }
     
+    
     return (
-        <div>
-            <h1>sign in</h1>
-            <button onClick={logGoogleUser}>
-                Sign in with Google Popup
-            </button>
-        </div>
-    )
+      <div>
+        <h1>sign in</h1>
+        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        <SignUpform/>
+      </div>
+    );
 }
 
 export default SignIn;
--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -3,7 +3,8 @@ import {
   getAuth, 
   signInWithRedirect, 
   signInWithPopup, 
-  GoogleAuthProvider 
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import {
   getFirestore,
@@ -23,17 +24,21 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
-    prompt: 'select_account'
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWhithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWhithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWhithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+  if (!userAuth) return;
+  
   const userDocRef = doc(db, 'users', userAuth.uid);
   const userSnapshot = await getDoc(userDocRef);
 
@@ -47,6 +52,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        ...additionalInformation,
       });
     } catch (error) {
       console.log('error creating the user', error.message);
@@ -55,3 +61,9 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   // 유저 데이터 존재
   return userDocRef;
 };
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if(!email || !password) return;
+  
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
