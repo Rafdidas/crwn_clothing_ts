4e92a1be3bcedda26828a5ba7a307c7c16487afa
HyeonGyu <yhu930421@naver.com>
2024-09-22 20:52:12 +0900

    로그인 구성요소 및 버튼 컴포넌트 추가

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import './sign-up-form.styles.scss';
+
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
 
 const defaultFormFields = {
     displayName: '',
@@ -48,11 +52,12 @@ const SignUpform = () => {
     };
 
     return (
-      <div>
-        <h1>Sign up with your email and password</h1>
+      <div className="sign-up-container">
+        <h2>Don't have an account?</h2>
+        <span>Sign up with your email and password</span>
         <form onSubmit={handleSubmit}>
-          <label>Display Name</label>
-          <input
+          <FormInput
+            label="Display Name"
             type="text"
             required
             onChange={handleChange}
@@ -60,8 +65,8 @@ const SignUpform = () => {
             value={displayName}
           />
 
-          <label>Email</label>
-          <input
+          <FormInput
+            label="Email"
             type="email"
             required
             onChange={handleChange}
@@ -69,8 +74,8 @@ const SignUpform = () => {
             value={email}
           />
 
-          <label>Password</label>
-          <input
+          <FormInput
+            label="Password"
             type="password"
             required
             onChange={handleChange}
@@ -78,15 +83,15 @@ const SignUpform = () => {
             value={password}
           />
 
-          <label>Confirm Password</label>
-          <input
+          <FormInput
+            label="Confirm Password"
             type="password"
             required
             onChange={handleChange}
             name="confirmPassword"
             value={confirmPassword}
           />
-          <button type="submit">Sign Up</button>
+          <Button type="submit">Sign Up</Button>
         </form>
       </div>
     );
