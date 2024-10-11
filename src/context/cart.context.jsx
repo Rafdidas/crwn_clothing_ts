82facee489250fe3fce79f193e2a9d0836c7930d
hue <hue@wisa.co.kr>
2024-10-11 16:08:39 +0900

    checkout 페이지 레이아웃 구성

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -16,12 +16,34 @@ export const addCartItem = (cartItems, productToAdd) => {
     return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    // 이미 존재 하고 있는 상품인지 
+    // 수량이 1인지 확인하고 1이면 카트에서 제거
+    // 아니라면 감소
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    );
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+}
+
+const clearCartItem = (cartItems, cartItemToClear) => {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+}
+
 export const CartContext = createContext({
     isCartOpen: false,     // 장바구니가 열려 있는지 여부
     setIsCartOpen: () => {}, // 장바구니 열기/닫기 상태를 설정하는 함수
     cartItems: [],         // 장바구니에 담긴 상품 목록
     addItemToCart: () => {}, // 장바구니에 상품을 추가하는 함수
     removeItemFromCart: () => {}, // 주문서에서 상품을 제거하는 함수
+    clearItemFromCart: () => {},
     cartCount: 0,
 });
 
@@ -39,11 +61,20 @@ export const CartProvider = ({children}) => {
         setCartItems(addCartItem(cartItems, productToAdd)); // 상품 추가
     }
 
+    const removeItemToCart = (cartItemToRemove) => {
+      setCartItems(removeCartItem(cartItems, cartItemToRemove)); 
+    };
+
+    const clearItemToCart = (cartItemToClear) => {
+      setCartItems(clearCartItem(cartItems, cartItemToClear)); 
+    };
+
     const value = {
       isCartOpen,
       setIsCartOpen,
       cartItems,
       addItemToCart,
+      removeItemToCart,
       cartCount,
     }; // 전역 상태 값
     return (
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,28 +3,45 @@ import './checkout.styles.scss';
 import { useContext } from 'react';
 import { CartContext } from '../../context/cart.context';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 const Checkout = () => {
     
-    const { cartItems, addItemToCart } = useContext(CartContext);
+    const { cartItems, addItemToCart, removeItemToCart } = useContext(CartContext);
     
     return (
-        <div>
-            <h1>Check Out</h1>
-            <div>
-                {
-                    cartItems.map((cartItem) => {
-                        const { id, name, quantity } = cartItem;
-                        return(
-                            <div key={id}>
-                                <span>{name}</span>
-                                <span>{quantity}</span>
-                                <span>decrement</span>
-                                <span onClick={() => addItemToCart(cartItem)}>increment</span>
-                            </div>
-                        )
-                    } )
-                }
+        <div className='checkout-container'>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div className='header-block'>
+                    <span>Description</span>
+                </div>
+                <div className='header-block'>
+                    <span>Quantity</span>
+                </div>
+                <div className='header-block'>
+                    <span>Price</span>
+                </div>
+                <div className='header-block'>
+                    <span>Remove</span>
+                </div>
             </div>
+            
+            {
+                cartItems.map((cartItem) => {
+                    return (
+                      <CheckoutItem
+                        key={cartItem.id}
+                        cartItem={cartItem}
+                        addItemToCart={addItemToCart}
+                        removeItemToCart={removeItemToCart}
+                      />
+                    );
+                } )
+            }
+            <span className='total'>Total: 0</span>
         </div>
     )
 }
